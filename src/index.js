3ae723abd3232a279f2ddeab2f258d5bdcf2cf13
Filmon H Abraha <abraha.filmon@gmail.com>
2022-11-17 06:30:48 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import "./styles.css";
+
+// document.getElementById("app").innerHTML = `
+// <h1>Hello Vanilla!</h1>
+// <div>
+//   We use the same configuration as Parcel to bundle this sandbox, you can find more
+//   info about Parcel
+//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// </div>
+// `;
+
+// function load_home() {
+//   document.getElementById("app").innerHTML =
+//     '<object class="loader" type="text/html" data="home.html"></object>';
+// }
+// load_home();
+
+let mindNav = document.querySelector("#mindNav");
+let bodyNav = document.querySelector("#bodyNav");
+let foodNav = document.querySelector("#foodNav");
+let financeNav = document.querySelector("#financeNav");
+let careerNav = document.querySelector("#careerNav");
+let right = document.querySelector(".right");
+
+// mindNav.addEventListener("click", () => {
+//   console.log("clicked mindNav");
+
+//   // $(".right").load("finance.html", ()=>{
+//   //   alert('loaded html')
+
+//   });
+
+// });
